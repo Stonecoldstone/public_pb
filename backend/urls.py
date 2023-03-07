2a20e34f8670866ecdefb70bf3b15da2e6d240f5
stone <shalfeyyy1@gmail.com>
2023-03-07 02:18:44 +0200

    quick fix to resolve django errors

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',                include('base.urls')),
+    # path('',                include('base.urls')),
     path('admin/',          admin.site.urls),
     path('api/users/',      include('base.urls.user_urls')),
     path('api/creators/',   include('base.urls.creator_urls')),
